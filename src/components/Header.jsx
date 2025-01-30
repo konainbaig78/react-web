fac927759fc08f0100f7eaff0687a141484f334d
Konain Baig <konainbaig915@gmail.com>
2025-01-30 15:51:51 +0500

    styled UI :lipstick:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from '../assets/images/LOGO.png'
+import group from '../assets/images/Group 11 1.png'
 
 const navigation = [
   { name: 'Home', href: '#' },
@@ -13,6 +14,7 @@ const navigation = [
 ]
 
 export default function Navbar() {
+  const  [active, setActive]= useState("Home")
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
@@ -40,14 +42,21 @@ export default function Navbar() {
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="hidden lg:flex align-middle lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-[#5E3BEE]  border-[#5E3BEE] border-1 w-[120px] h-[30px] bg-white">
+      {navigation.map((item) => (
+        <a
+          key={item.name}
+          href={item.href}
+          onClick={() => setActive(item.name)}
+          className={`text-sm font-semibold transition duration-300 ${
+            active === item.name ? "text-[#5E3BEE] text-500 font-bold" : "text-gray-500"
+          }`}
+        >
+          {item.name}
+        </a>
+      ))}
+    </div>
+          <div className=" hidden lg:flex align-middle lg:flex-1 lg:justify-end">
+            <a href="#" className="text-sm/6 font-semibold text-[#5E3BEE]  flex items-center justify-center text-center border-[#5E3BEE] border-1 w-[109px] h-[43px] bg-white">
 
              Contact me 
             </a>
@@ -114,36 +123,37 @@ export default function Navbar() {
             className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
+        <div className='flex items-center justify-evenly '>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+          
+          <div className=" ">
+          <div className="relative text-[21px]  leading-[32px] font-semibold text-[#282938] text-left text-sm/6 text-[#282938] ">
+            Hey, I am John
             </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+            <span className=" !text-[61px] leading-[75px] font-bold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            I create <span className='text-[#5E3BEE]'>product design</span> and brand experience
+            </span>
+            <p className="mt-8 leading-[38px] !text-[23px] font-normal text-pretty text-gray-500 sm:text-xl/8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div className="mt-10 flex items-center  gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-[5.3px] bg-[#5E3BEE] text-[16px]
+ px-3.5 py-2.5 text-sm font-light text-white shadow-xs  w-[124px] h-[46px] flex items-center justify-center"
               >
-                Get started
+                Get In Touch
               </a>
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+              
             </div>
           </div>
+        </div>
+        <div>
+          <img src={logo} alt="logo" className='  w-[191px] h-[40px]' />
+        </div>
+        <div>
+          <img src={group} alt="group"  />
+        </div>
         </div>
         <div
           aria-hidden="true"
